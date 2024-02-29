41e93037c3912c9e561f6992ea69a95fcc3d4be0
Guilherme Alves <iamalves.jpg@gmail.com>
2024-02-29 00:51:12 -0300

    feat(routes):set up routes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './src/routes';
+import {StatusBar} from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#36393F" barStyle={'light-content'} />
+      <Routes />
+    </NavigationContainer>
   );
 }
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+export default function Login() {
+    return (
+        <SafeAreaView>
+            <Text>Login</Text>
+        </SafeAreaView>
+    );
+}
